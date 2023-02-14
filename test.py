f058ce51110d2a24499e0ce8420f90b7fc16f05a
3103 Dev <3103Dev@HKATechnologies.com>
2023-02-13 21:33:47 -0500

    Update to try upstream push

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 import tkinter as tk
+import time
+import sys
+import RPi.GPIO as GPIO
+import smbus
+import fcntl
 from tkinter import *
+
+I2C_BUS = 1
+CHIP00 = 0x38
+CHIP01 = 0x39
+CHIP02 = 0x3a
+CHIP03 = 0x3b
+CHIPS = {0x38,0x39,0x3a,0x3b}
 
 class Window(Frame):
   def __init__(self, master=None):
@@ -30,6 +42,9 @@ root.title("My GUI")
 #root.geometry("320x200")
 root.geometry("800x480")
 
+# I2C Bus setup
+bus = smbus.SMBus(I2C_BUS)
+testRead = bus.read_byte_data(CHIP00, 0x00)
 
 # Run forever!
 root.mainloop()
