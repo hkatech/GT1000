04a5a09877f9a264145105b208853ef9c4c12ae6
3103 Dev <3103Dev@HKATechnologies.com>
2023-02-19 23:29:23 -0500

    Define updates

--- GT1000.py
+++ GT1000.py
@@ -17,6 +17,8 @@ class GT1000:
   CHIP110 = 0x3e
   CHIP111 = 0x3f
   CHIPS = [CHIP000,CHIP001,CHIP010,CHIP011,CHIP100,CHIP101,CHIP110,CHIP111]
+  INCHIPS = [CHIP000]
+  OUTCHIPS = [CHIP010,CHIP011,CHIP100,CHIP110]
 
   # Bank selects
   BANK1 = 21
@@ -26,21 +28,20 @@ class GT1000:
   BANKS = [BANK1,BANK2,BANK3,BANK4]
 
   # GPIO Inputs/Outputs
-  EXT_IN_START = 13
-  EXT_IN_ABORT = 19
-  EXT_IN_USER2 = 6
-  EXT_IN_LEAK = 26
+  EXT_IN_START = 6
+  EXT_IN_ABORT = 13
+  EXT_IN_PART2 = 19
 
-  EXT_OUT_PASS = 18
-  EXT_OUT_FAIL = 15
+  EXT_OUT_PASS = 8
+  EXT_OUT_FAIL = 24
   EXT_OUT_INTEST = 23
-  EXT_OUT_USER1 = 8
-  EXT_OUT_START = 18
-  EXT_OUT_ABORT = 15
+  EXT_OUT_LATCHRELEASE = 14
+  EXT_OUT_START = 23
+  EXT_OUT_ABORT = 18
 
   GT_Start = False
   GT_Abort = False
-  GT_UserIn = False
+  GT_PartSelect = False
   GT_LK = False
   GT_Pass = False
   GT_Fail = False
@@ -59,6 +60,25 @@ class GT1000:
   GTchip7int = [0,0,0]
   GTchip8int = [0,0,0]
 
+  # Stations
+  testStations = [\
+    ["Station 1",0x3a,0],\
+    ["Station 2",0x3a,1],\
+    ["Station 3",0x3a,2],\
+    ["Station 4",0x3a,3],\
+    ["Station 5",0x3a,4],\
+    ["Station 6",0x3a,5],\
+    ["Station 7",0x3a,6],\
+    ["Station 8",0x3a,7],\
+    ["Station 9",0x3b,0],\
+    ["Station 10",0x3b,1],\
+    ["Station 11",0x3b,2],\
+    ["Station 12",0x3b,3],\
+    ["Station 13",0x3b,4],\
+    ["Station 14",0x3b,5],\
+    ["Station 15",0x3b,6],\
+    ["Station 16",0x3b,7]]
+
   stationChips = [\
     CHIP000, CHIP000, CHIP000, CHIP000, CHIP000, \
     CHIP000, CHIP000, CHIP000, CHIP000, CHIP000, \
@@ -70,14 +90,12 @@ class GT1000:
     print("[call] __GPIO_Setup()")
     GPIO.setup(self.EXT_IN_START, GPIO.IN)
     GPIO.setup(self.EXT_IN_ABORT, GPIO.IN)
-    GPIO.setup(self.EXT_IN_USER2, GPIO.IN)
-    GPIO.setup(self.EXT_IN_LEAK, GPIO.IN)
+    GPIO.setup(self.EXT_IN_PART2, GPIO.IN)
 
-    # Pass/Fail map to LTStart/LTFail
-    GPIO.setup(self.EXT_OUT_PASS, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(self.EXT_OUT_FAIL, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(self.EXT_OUT_START, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(self.EXT_OUT_ABORT, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(self.EXT_OUT_INTEST, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(self.EXT_OUT_USER1, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(self.EXT_OUT_LATCHRELEASE, GPIO.OUT, initial=GPIO.LOW)
     print("[end call] __GPIO_Setup()")
 
 
@@ -85,17 +103,13 @@ class GT1000:
     print("[call] read_inputs()")
     GT_Start = GPIO.input(self.EXT_IN_START)
     GT_Abort = GPIO.input(self.EXT_IN_ABORT)
-    GT_UserIn = GPIO.input(self.EXT_IN_USER2)
-    GT_LK = GPIO.input(self.EXT_IN_LEAK)
+    GT_PartSelect = GPIO.input(self.EXT_IN_PART2)
     try:
       self.GTchip0int[0] = self.__I2C.read_byte(self.CHIP000)
-      self.GTchip1int[0] = self.__I2C.read_byte(self.CHIP001)
-      self.GTchip2int[0] = self.__I2C.read_byte(self.CHIP010)
-      self.GTchip3int[0] = self.__I2C.read_byte(self.CHIP011)
-      self.GTchip4int[0] = self.__I2C.read_byte(self.CHIP100)
-      self.GTchip5int[0] = self.__I2C.read_byte(self.CHIP101)
-      self.GTchip6int[0] = self.__I2C.read_byte(self.CHIP110)
-      self.GTchip7int[0] = self.__I2C.read_byte(self.CHIP111)
+      #self.GTchip2int[0] = self.__I2C.read_byte(self.CHIP010)
+      #self.GTchip3int[0] = self.__I2C.read_byte(self.CHIP011)
+      #self.GTchip4int[0] = self.__I2C.read_byte(self.CHIP100)
+      #self.GTchip6int[0] = self.__I2C.read_byte(self.CHIP110)
     except Exception as e:
       print("!!! ", str(e), " !!!")
       pass
@@ -106,6 +120,7 @@ class GT1000:
     self.__I2C.write_byte_data(addy, 1, data)
 
   def resetAllOutputs(self):
+    print("[call] resetAllOutputs()")
     #self.GTchip0int[0] = 0
     #self.GTchip1int[0] = 0
     #self.GTchip2int[0] = 0
@@ -114,9 +129,13 @@ class GT1000:
     #self.GTchip5int[0] = 0
     #self.GTchip6int[0] = 0
     #self.GTchip7int[0] = 0
-    for val in self.CHIPS:
-      self.write_output(val,0)
-
+    try:
+      for val in self.OUTCHIPS:
+        self.write_output(val,0)
+    except Exception as e:
+      print("!!! ", str(e), " !!!")
+      pass
+    print("[end call] resetAllOutputs()")
 
   def startTest(self):
     startFunction = time.time()
@@ -153,4 +172,6 @@ class GT1000:
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     self.__GPIO_Setup()
+    self.resetAllOutputs()
     self.read_inputs()
+
